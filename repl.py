f0e3e1882204e69993ea257bef5365ca4e1bbd13
TheFerguson99 <63678756+TheFerguson99@users.noreply.github.com>
2024-07-20 19:56:54 +0200

    Update repl.py

--- repl.py
+++ repl.py
@@ -1,6 +1,8 @@
 from cll.NVMStorage import NVMStorage # type: ignore
 import microcontroller
 settings = NVMStorage(size=20)
+repl_remove = settings.get_key("repl_remove")
 DEBUG = settings.get_key("DEBUG")
-if not DEBUG:
+if repl_remove and DEBUG == False:
+    print("I dont think so.")
     microcontroller.reset()
