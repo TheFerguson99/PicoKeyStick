80ce37c0bad6f757a78b8a298973cdc55a2735e5
TheFerguson99 <63678756+TheFerguson99@users.noreply.github.com>
2024-07-25 17:43:54 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -4,25 +4,35 @@ import storage
 
 from cll.NVMStorage import NVMStorage # type: ignore
 
-
-settings = NVMStorage(size=20)
-first_run = settings.get_key("init")
+settings = CSVHandler(file_path="/data/settings.csv")
+config = NVMStorage(size=20)
+first_run = config.get_key("init")
 #Check if first run
 if first_run == False:
-    settings.write_key("DEBUG",True)
-    settings.write_key("show_usb",True)
-    settings.write_key("init",True)
-DEBUG = settings.get_key("DEBUG")
-show_usb = settings.get_key("show_usb")
-boot_test = settings.get_key("boot_test")
+    config.write_key("DEBUG",True)
+    config.write_key("show_usb",True)
+    config.write_key("init",True)
+DEBUG = config.get_key("DEBUG")
+show_usb = config.get_key("show_usb")
+boot_test = config.get_key("boot_test")
 #TODO: Remove REPL. done bye restart when accesing. Find better way???
 
 if boot_test == True:
     pass
 
 storage.remount("/", readonly=False)
+try:
+    files = os.listdir("/temp/")
+    for file in files:
+        try:
+            os.remove("/temp/" + file)
+        except:
+            if DEBUG:
+                print("Failed to delete file :" + file)
+except OSError as e:
+    print("Error:", e)
 m = storage.getmount("/")
-m.label = os.getenv("drive_name")
+m.label = settings.get_value_by_name("drivename")
 storage.remount("/", readonly=DEBUG)
 if show_usb == False:
     storage.disable_usb_drive()
