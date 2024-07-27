a082c245433e0be45d34015c620de3e4e63833b4
TheFerguson99 <63678756+TheFerguson99@users.noreply.github.com>
2024-07-27 17:55:31 +0200

    Update settings.py

--- cll/settings.py
+++ cll/settings.py
@@ -2,5 +2,9 @@ settings = {
     'delay':'int',
     'drivename':'str',
     'master_key':'str',
-    'auto_payload_filename':'str'
+    'auto_payload_filename':'str',
+    'AREA_WIDTH':'int',
+    'AREA_HEIGHT':'int',
+    'STEP':'int',
+    'DELAY':'float',
 }
